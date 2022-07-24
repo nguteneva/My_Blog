744160e30d8bd5975cdc7b5151aa6537de0f9c10
Nataliya Guteneva <nguteneva@gmail.com>
2022-07-24 22:18:37 +0300

    Add gunicorn and Procfile

--- server.py
+++ server.py
@@ -6,8 +6,7 @@ import requests
 from flask import request
 
 
-my_email = "nat_666@list.ru"
-password = 'tGtG5Hw3TsWyNHLdNWdw'
+
 link = "https://api.npoint.io/5a53af3908a610cb9f70"
 app = Flask(__name__)
 data = requests.get(link).json()
@@ -32,11 +31,6 @@ def form_entry():
         email = request.form['email']
         phone = request.form['phone']
         message = request.form['message']
-        with smtplib.SMTP("smtp.mail.ru") as connection:
-            connection.starttls()
-            connection.login(user=my_email, password=password)
-            connection.sendmail(from_addr=my_email, to_addrs='nguteneva@gmail.com',
-                                msg=f"Subject:You have a new message from the blog\n\nName: {name}\nemail: {email}\nPhone:{phone}\nMessage:{message}")
         return render_template('form-entry.html')
     else:
         return render_template('contact.html')
